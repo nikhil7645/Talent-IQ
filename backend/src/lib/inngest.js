69a7cfbe2c186b8d0248ca48c24f7426cb3774ca
Nikhil Kumar <kumarnikhil95861@gmail.com>
2025-11-20 18:43:19 +0530

    updated id needed for ingest

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -2,7 +2,7 @@ import {Inngest} from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
 
-export const inngest = new Inngest({ name: "talent-iq" });
+export const inngest = new Inngest({ id: "talent-iq" });
 
 const syncUser = inngest.createFunction(
     {id:"sync/user"},
