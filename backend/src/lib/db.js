1541dd33b995ec6f260b24f4a426f9dd338b42ac
Nikhil Kumar <kumarnikhil95861@gmail.com>
2025-11-20 15:46:39 +0530

    fix: added env check for DB URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,6 +4,9 @@ import {ENV} from "./env.js";
 
 export const connectDB = async()=>{
     try{
+        if(!ENV.DB_URL){
+            throw new Error("Database URL is not defined in environment variables");
+        }
         const conn = await moongoose.connect(ENV.DB_URL);
         console.log("Database connected successfully" , conn.connection.host);
     }catch(err){
