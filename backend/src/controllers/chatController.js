a99c3c4a6138963405b79f3715f5fd308ee2f5a1
Nikhil Kumar <kumarnikhil95861@gmail.com>
2025-12-19 01:12:08 +0530

    server side auth implemented

---
+++ backend/src/controllers/chatController.js
@@ -0,0 +1,17 @@
+import { chatClient } from "../lib/stream.js";
+
+export async function getStreamToken(req, res) {
+    try{
+        //use clerkid for stream (not mongodb id) => it should match the id in stream dashboard
+        const token = chatClient.createToken(req.user.clerkID);
+        res.status(200).json({
+            token,
+            userId : req.user.userId,
+            userName: req.user.name,
+            userImage: req.user.image
+        })
+    } catch (error){
+        console.log("Error in getStreamToken controller: ",error);
+        req.status(500).json({message : "Internet serveer error"});
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,21 +5,23 @@ import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest , functions } from "./lib/inngest.js";
 import { serve } from "inngest/express"; 
+import { clerkMiddleware } from '@clerk/express'
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 const __dirname = path.resolve();
 
 //middleware
 app.use(express.json());
+app.use(clerkMiddleware());  //this adds auth field to req object: req.auth()
+
 //credentials true to allow cookies from frontend in request
 app.use(cors({origin:ENV.CLIENT_URL , credentials:true}));
 app.use("/api/inngest" , serve({client:inngest, functions}));
+app.use("/api/chat", chatRoutes);
 
 app.get("/health" , (req,res)=>{
     res.status(200).json({msg:"api is up and running"});
-})
-app.get("/books" , (req,res)=>{
-    res.status(200).json({msg:"book api is up and running"});
 })
 
 //make ready for deployement
